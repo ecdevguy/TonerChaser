7460010c40731330e5d12b9e3ebad5ccac8cdb3f
Elias Cleveland <eliascleveland1@outlook.com>
2024-03-26 19:47:10 +0800

    study component updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,22 @@ import { tocflOne } from './vocabulary/tocfl-1'
 function App() {
   const [currentView, setCurrentView] = React.useState('home');
   const [vocab, setVocab] = React.useState(() => JSON.parse(localStorage.getItem("tocfl")) || tocflOne)
-  
+  const [currentCharacter, setCurrentCharacter] = React.useState()
+  const [index, setIndex] = React.useState(0);
+
+  const increaseIndex = () => {
+    setIndex(prevIndex => (prevIndex + 1) % vocab.length);
+  };
+
+  const decreaseIndex = () => {
+    setIndex(prevIndex => (prevIndex - 1 + vocab.length) % vocab.length);
+  };
+
+  React.useEffect(() => {
+    setCurrentCharacter(vocab[index]);
+  }, [index, vocab]);
+
+
   React.useEffect(() => {
     localStorage.setItem("tocfl", JSON.stringify(tocflOne))
   }, [])
@@ -26,13 +41,13 @@ function App() {
       {currentView === 'home' && (
         <Homescreen handleClick={handleViewChange}/>
       )}
+
+      {currentView !== 'home' && <button onClick={() => handleViewChange('home')}>Go Home</button>}
       
-      {currentView === 'study' && <Study />}
+      {currentView === 'study' && <Study currentCharacter={currentCharacter} handlePrevious={decreaseIndex} handleNext={increaseIndex}/>}
       {currentView === 'challenge' && <Challenge />}
       {currentView === 'list' && <List currentVocab={vocab}/>}
       {currentView === 'settings' && <Settings />}
-      
-      {currentView !== 'home' && <button onClick={() => handleViewChange('home')}>Go Home</button>}
 
     </div>
     )
--- src/components/Study.jsx
+++ src/components/Study.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Character from './Character'
 
-export default function Study() {
+export default function Study(props) {
   return(
-    <h1>study mode</h1>
+    <>
+      <h1>study mode</h1>
+      <Character 
+        word={props.currentCharacter.Word}
+        pinyin={props.currentCharacter.Pinyin}
+        otherPinyin={props.currentCharacter.OtherPinyin}
+        level={props.currentCharacter.Level}
+        firstTranslation={props.currentCharacter["First Translation"]}
+      />
+      <button onClick={() => props.handlePrevious()}>Previous</button>
+      <button onClick={() => props.handleNext()}>Next</button>
+    </>
   )
 }
