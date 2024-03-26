290b28b27d4bbe9f13d52f4b4dadfd96029f7bb4
Elias Cleveland <eliascleveland1@outlook.com>
2024-03-26 17:08:26 +0800

    file formatting

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,7 +1,5 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
-
-
 import App from '../App.jsx';
 
 describe('App Component', () => {
