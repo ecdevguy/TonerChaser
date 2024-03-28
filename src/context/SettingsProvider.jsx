258ba071c5b31444e6c44437191003fd78d4cfba
Elias Cleveland <eliascleveland1@outlook.com>
2024-03-28 17:59:14 +0800

    settings save to local

--- src/context/SettingsProvider.jsx
+++ src/context/SettingsProvider.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SettingsContext from './settingsContext'; 
 
 export default function SettingsProvider({ children }) {
-    const [userSettings, setUserSettings] = useState({
-        audio: false,
-        darkMode: false,
-        tocfl1: true,
-        tocfl2: false,
-        tocfl3: false
+    const [userSettings, setUserSettings] = useState(() => {
+        const localSettings = localStorage.getItem('userSettings');
+        return localSettings ? JSON.parse(localSettings) : {
+            audio: false,
+            darkMode: false,
+            tocfl1: true,
+            tocfl2: false,
+            tocfl3: false
+        };
     });
+
+    useEffect(() => {
+        localStorage.setItem('userSettings', JSON.stringify(userSettings));
+    }, [userSettings]);
 
     const value = { userSettings, setUserSettings };
 
