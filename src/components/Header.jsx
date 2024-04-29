c1098de5a1148b3f32f7fff90f2ce1da95dab393
Elias Cleveland <eliascleveland1@outlook.com>
2024-04-29 17:53:22 +0800

    mui checkpoint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,53 @@
 import React from 'react'
 import { Link as RouterLink } from 'react-router-dom'
-import { AppBar, Link, Toolbar, styled } from '@mui/material'
+import { AppBar, Box, Link, Toolbar, Typography, styled } from '@mui/material'
+import { HomeRounded } from '@mui/icons-material'
 
 const StyledToolbar = styled(Toolbar) ({
   display:"flex",
   justifyContent:"space-between",
-  color:"blue"
+  color:"white"
 })
+
+const Search = styled("div")(({theme})=>({
+  backgroundColor:"white",
+  padding:"0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width:"20%"
+}))
+
+const Features = styled(Box)(({theme})=>({
+  
+}))
 
 
 function Header() {
 
   return (
-    <AppBar position="stick">
-      <StyledToolbar>
-        this works
-        <Link component={RouterLink} underline="none" className="site-logo" to="/" color="inherit">This does work</Link>
-        <Link component={RouterLink} underline="hover" to="study">study</Link>
-        <Link component={RouterLink} underline="hover" to="challenge">challenge</Link>
-        <Link component={RouterLink} underline="hover" to="list">list</Link>
-        <Link component={RouterLink} underline="hover" to="settings">settings</Link>
+    <AppBar position="sticky">
+      <StyledToolbar> 
+        <Link 
+        component={RouterLink} underline="none" className="site-logo" to="/" color="inherit">
+          <Typography variant='h6' sx={{display:{xs:"none", sm:"block"}}}>
+            TONECHASER
+          </Typography>
+          <HomeRounded sx={{display:{xs:"block", sm:"none"}}}/>
+        </Link>
+        <Search>Search</Search>
+        <Features>
+          <Link component={RouterLink} underline="hover" to="study" color="inherit">
+            <Typography>Study</Typography>
+          </Link>
+          <Link component={RouterLink} underline="hover" to="challenge" color="inherit">
+            <Typography>Challenge</Typography>
+          </Link>
+          <Link component={RouterLink} underline="hover" to="list" color="inherit">
+            <Typography>List</Typography>
+          </Link>
+          <Link component={RouterLink} underline="hover" to="settings" color="inherit">
+            <Typography>Settings</Typography>
+          </Link>
+        </Features>
       </StyledToolbar>
     </AppBar>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import SettingsProvider from './context/SettingsProvider'
-// import './index.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SettingsProvider>
       <App />
     </SettingsProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
