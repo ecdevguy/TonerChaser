a51ca83181f73a7a668fe3d8d4f674e837fed969
Elias Cleveland <eliascleveland1@outlook.com>
2024-04-29 14:54:47 +0800

    mui checkpoint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
+import { AppBar, Link, Toolbar, styled } from '@mui/material'
+
+const StyledToolbar = styled(Toolbar) ({
+  display:"flex",
+  justifyContent:"space-between",
+  color:"blue"
+})
+
 
 function Header() {
+
   return (
-    <header>
-      <Link className="site-logo" to="/">ToneChaser</Link>
-      <nav>
-        <NavLink to="study">study</NavLink>
-        <NavLink to="challenge">challenge</NavLink>
-        <NavLink to="list">list</NavLink>
-        <NavLink to="settings">settings</NavLink>
-      </nav>
-    </header>
+    <AppBar position="stick">
+      <StyledToolbar>
+        this works
+        <Link component={RouterLink} underline="none" className="site-logo" to="/" color="inherit">This does work</Link>
+        <Link component={RouterLink} underline="hover" to="study">study</Link>
+        <Link component={RouterLink} underline="hover" to="challenge">challenge</Link>
+        <Link component={RouterLink} underline="hover" to="list">list</Link>
+        <Link component={RouterLink} underline="hover" to="settings">settings</Link>
+      </StyledToolbar>
+    </AppBar>
   )
 }
 
 export default Header
+
+{/* <Link component={RouterLink} underline="none" className="site-logo" to="/">ToneChaser</Link>
+<Link component={RouterLink} underline="hover" to="study">study</Link>
+<Link component={RouterLink} underline="hover" to="challenge">challenge</Link>
+<Link component={RouterLink} underline="hover" to="list">list</Link>
+<Link component={RouterLink} underline="hover" to="settings">settings</Link> */}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Box } from "@mui/material";
 
 export default function Layout() {
   return (
-    <div>
+    <Box>
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </div>
+    </Box>
   )
 }
