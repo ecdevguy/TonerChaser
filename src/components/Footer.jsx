12ca775228989ae95152893658a5050f5e3ca14f
Elias Cleveland <eliascleveland1@outlook.com>
2024-06-03 20:37:41 +0800

    footer code cleanup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,27 +3,32 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <Paper sx={{marginTop: 'calc(10% + 60px)',
+    <Paper 
+      sx={{marginTop: 'calc(10% + 60px)',
       position: 'fixed',
       bottom: 0,
       width: '100%'
       }} 
       component="footer" 
-      square variant="outlined"><Container maxWidth="lg">
-     
-      <Box
-        sx={{
-          flexGrow: 1,
-          justifyContent: "center",
-          display: "flex",
-          m: 0.5,
-        }}
-      >
-        <Typography variant="caption" color={"text.primary"}>
-          last updated: May, 2024
-        </Typography>
-      </Box>
-    </Container></Paper>
+      square variant="outlined"
+    >
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            flexGrow: 1,
+            justifyContent: "center",
+            display: "flex",
+            m: 0.5,
+          }}
+        >
+          <Typography 
+            variant="caption" 
+            color={"text.primary"}>
+            last updated: June, 2024
+          </Typography>
+        </Box>
+      </Container>
+    </Paper>
   )
 }
 
